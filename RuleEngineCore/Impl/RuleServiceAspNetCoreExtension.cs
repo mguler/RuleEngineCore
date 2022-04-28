49c89277c212dc027eade50cf2526a697feaf7ca
Jackson <Jackson@DESKTOP-TF6VKBJ>
2022-04-28 12:15:25 +0300

    bugfixes added to extension method

--- RuleEngineCore/Impl/RuleServiceAspNetCoreExtension.cs
+++ RuleEngineCore/Impl/RuleServiceAspNetCoreExtension.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RuleEngineCore.Abstract;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RuleEngineCore.Impl
 {
     public static class RuleServiceAspNetCoreExtension
     {
-        public static void AddRuleService(this IServiceCollection services, params Type[] ruleConfigurationTypes) 
+        public static void AddRuleService(this IServiceCollection services, params Type[] ruleConfigurationTypes)
         {
+            var containsNull = ruleConfigurationTypes.Any(type => type == null);
+            if (containsNull)
+            {
+                throw new ArgumentException("Method does not allow nulls");
+            }
+
             services.AddSingleton<IRuleServiceProvider>((serviceProvider) =>
             {
-                var instances = ruleConfigurationTypes.Select(type => type.GetConstructor(Type.EmptyTypes).Invoke(null) as IRuleSet).ToArray();
+                var instances = CreateInstances(ruleConfigurationTypes).ToArray();
+
                 var ruleService = new RuleServiceProviderDefaultImpl(instances);
                 ruleService.SetDependencyResolver(type => serviceProvider.CreateScope().ServiceProvider.GetRequiredService(type));
                 return ruleService;
@@ -25,6 +33,18 @@ namespace RuleEngineCore.Impl
                 var ruleService = new RuleServiceProviderDefaultImpl(ruleConfigurations);
                 return ruleService;
             });
+        }
+        private static IEnumerable<IRuleSet> CreateInstances(Type[] types)
+        {
+            foreach (var type in types)
+            {
+                var instance = type.GetConstructor(Type.EmptyTypes).Invoke(null) as IRuleSet;
+                if (instance is null)
+                {
+                    throw new Exception($"the type {type} does not implement {nameof(IRuleSet)}");
+                }
+                yield return instance;
+            }
         }
     }
 }
